7272b050b359f54e269f061652a6337f22845cc3
rohan17nagar <rohan17nagar@gmail.com>
2025-09-21 16:14:46 +0530

    java-streams: stream api getSquareOfEach

--- src/streams/CountEachCharacter.java
+++ src/streams/CountEachCharacter.java
@@ -5,12 +5,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 
-class sumClass{
-    public static int sum(int a,int b){
-        return a+b;
+class sumClass {
+    public static int sum(int a, int b) {
+        return a + b;
     }
-    public static int subtract(int a,int b){
-        return a-b;
+
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
 
@@ -22,15 +23,15 @@ public class CountEachCharacter {
         String string = "Iam very very very good good and good good person";
 
         /*
-        * Map the char count
+         * Map the char count
          * Condition:
          * 1. It should not show the space counts
          * 2. No character should be repeated as a key
          * 3. Characters should appear in sequential order.
-        * */
+         * */
         Map<Character, Integer> countChars = string.chars().
                 mapToObj(c -> (char) c)
-                .filter(c -> c !=' ')
+                .filter(c -> c != ' ')
                 .collect(Collectors.toMap(
                         c -> c,
                         c -> 1,
